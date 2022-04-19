0de7a09558d4222ea740bc57837d269d175e4c21
SonNT <son.nguyen@tda.company>
2022-04-19 22:16:06 +0700

    migrate

--- _docker/startup.sh
+++ _docker/startup.sh
@@ -5,8 +5,7 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
-
-php artisan migrate --force
-php artisan db:seed
+cd /app && php artisan migrate --force
+cd /app && php artisan db:seed
 
 nginx
